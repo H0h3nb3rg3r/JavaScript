e81d61f5ead970f347e6f9e8204a767bb0a4b5f2
Bruno Hohenberger <brunochapecoense14@gmail.com>
2026-03-11 14:42:28 -0300

    js

--- ex_casa/carrinho.js
+++ ex_casa/carrinho.js
@@ -3,4 +3,11 @@ export class Carrinho{
         this.cliente = cliente;
         this.item = []
     }
+    calcularTotal(){
+        let total = 0;
+        for(let i in this.itens){
+            total += this.itens[i];
+        }
+        return total;
+    }
 }
