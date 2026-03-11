17ea9959d8f9421ca8bf83d1924e46fbc627c4a7
Bruno Hohenberger <brunochapecoense14@gmail.com>
2026-03-11 15:13:51 -0300

    js

--- revisao07.js
+++ revisao07.js
@@ -3,3 +3,16 @@ let precoNovo = precos.map((item)=> item * 1.1);
 for(let i in precoNovo){
     console.log(precoNovo[i]);
 }
+ let notas = [
+    {nome:"Jackson Five", nota:10},
+    {nome:"Chaves", nota:8},
+    {nome:"Chiquinha", nota:6},
+    {nome:"Seu Madruga", nota:2},
+    {nome:"Kiko", nota:0},
+]; 
+let ap = notas.filter(function(item){
+    return item.nota >= 6;
+});
+ap.map(function(item){
+    console.log("Nome: "+item.nome+" - Nota:"+item.nota);
+});
