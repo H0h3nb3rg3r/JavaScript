cc021cae0b3d705663df19b82ff315a277f2f5fc
Bruno Hohenberger <brunochapecoense14@gmail.com>
2026-03-12 16:04:39 -0300

    js

--- ex_casa/carrinho.js
+++ ex_casa/carrinho.js
@@ -18,9 +18,13 @@ export class Carrinho{
     exibirResumo(){
         console.log("Cliente: "+this.cliente.nome);
         console.log("Telefone: "+this.cliente.telefone);
-        for(let i in this.itens){
+/*        for(let i in this.itens){
             console.log(this.itens[i].nome + " - R$"+this.itens[i].preco);
         }
+*/
+          this.itens.map(function(item){
+            console.log(item.nome + " - R$"+item.preco);
+          })  
         console.log("Total: R$"+this.calcularTotal().toFixed(2));
     }
 }
---
+++ revisao08.js
@@ -0,0 +1,15 @@
+import {Pessoa} from "./pessoa.js";
+import { Produto } from "./ex_casa/produto.js";
+import {Carrinho} from "./ex_casa/carrinho.js";
+
+let pessoa1 = new Pessoa("Jackson Five", 38, "49 8800-5500");
+let pessoa2 = new Pessoa("Chaves",8,"49 3300-1100");
+
+let produto1 = new Pessoa("Maça",7,20);
+let produto2 = new Pessoa("Chocolate",15,25);
+let produto3 = new Pessoa("Table",1500,2);
+
+let carrinho1 = new Carrinho(pessoa1);
+let carrinho2 = new Carrinho(pessoa2);
+
+ 
